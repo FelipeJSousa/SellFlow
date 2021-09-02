38e4b2079a921056051135ec4eb5bd93b1508ede
Felipe Sousa <csfelipe8@gmail.com>
2021-09-01 22:33:39 -0300

    Adcionado cpf

--- Entity/Pessoa.cs
+++ Entity/Pessoa.cs
@@ -14,6 +14,7 @@ namespace Entity
         public string nome { get; set; }
         public string sobrenome { get; set; }
         public string cpf { get; set; }
+        public string cpnj { get; set; }
         public DateTime dataNascimento { get; set; }
         public bool ativo { get; set; }
     }
