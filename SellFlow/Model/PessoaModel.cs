22c97a32044141c0d8f1b316ffee7a8177df4e8e
Felipe Sousa <csfelipe8@gmail.com>
2021-09-03 21:48:11 -0300

    Corrigido exposição do relacionamento de classes

--- SellFlow/Model/PessoaModel.cs
+++ SellFlow/Model/PessoaModel.cs
@@ -5,13 +5,12 @@ namespace SellFlow.Model
 {
     public class PessoaModel
     {
-        
         public int id { get; set; }
         public string nome { get; set; }
         public string sobrenome { get; set; }
         public string cpf { get; set; }
         public DateTime dataNascimento { get; set; }
         public bool ativo { get; set; }
-        public UsuarioModel usuario { get; set; }
+        
     }
 }
