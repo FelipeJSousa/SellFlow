f70f52bb06b54be027c2fa016fa3facef4e71cb6
Felipe Sousa <csfelipe8@gmail.com>
2021-09-07 11:37:01 -0300

    Adicionado repositorio endereco

---
+++ Repository/EnderecoRepository.cs
@@ -0,0 +1,24 @@
+﻿using Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repository
+{
+    public class EnderecoRepository : BaseRepository<Endereco>
+    {
+        public new Endereco Add(Endereco end)
+        {
+            using (_context = new AppDbContext())
+            {
+                end.ativo = true;
+                _context.Endereco.Add(end);
+                _context.SaveChanges();
+            }
+            Dispose();
+            return end;
+        }
+    }
+}
